9adc94ba60f303afe06c10119afce0a68b3a22a8
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-02-16 17:40:04 -0300

    feat: adiciona footer e navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
-
 import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
-
 export default App;
