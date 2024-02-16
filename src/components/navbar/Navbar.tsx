436195a426b586d7b33f71879b9befeab981c413
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-02-16 20:04:42 -0300

    feat: adiciona import do Link

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <>
@@ -6,6 +8,12 @@ function Navbar() {
           <div className="text-2xl font-bold uppercase">Blog Pessoal</div>
 
           <div className="flex gap-4">
+            <Link to="/login" className="hover:underline">
+              Login
+            </Link>
+            <Link to="/home" className="hover:underline">
+              Home
+            </Link>
             <div className="hover:underline">Postagens</div>
             <div className="hover:underline">Temas</div>
             <div className="hover:underline">Cadastrar tema</div>
