bcd950e93f044230fc6087a27113c5f15b79db45
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-02-16 20:04:20 -0300

    style: adiciona uso de aspas duplas

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import "./App.css";
 import Home from "./pages/home/Home";
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login";
+
 function App() {
   return (
-       <>
-    <BrowserRouter>
+    <>
+      <BrowserRouter>
         <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
