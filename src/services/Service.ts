8d497b40009c19c804361d05e1aef2edd537658e
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-02-19 16:26:39 -0300

    feat: adiciona configuração service

---
+++ src/services/Service.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://blogpessoal-c5cb.onrender.com",
+});
+
+export const cadastrarUsuario = async (
+  url: string,
+  dados: Object,
+  setDados: Function
+) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
+
+export const login = async (url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
