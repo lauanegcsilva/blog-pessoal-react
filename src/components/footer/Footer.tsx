49b56724a15f1ed24d94fb04e426b36a6d779557
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-02-21 15:45:10 -0300

    fix: altera footer para Lauane

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,9 +10,9 @@ function Footer() {
       <div className="flex justify-center bg-indigo-900 text-white">
         <div className="container flex flex-col items-center py-4">
           <p className="text-xl font-bold">
-            Blog pessoal Generation | Copyright:{" "}
+            Blog pessoal Lauane | Copyright:{" "}
           </p>
-          <p className="text-lg">Acesse nossas redes sociais</p>
+          <p className="text-lg">Acesse minhas redes sociais</p>
           <div className="flex gap-2">
             <LinkedinLogo size={48} weight="bold" />
             <InstagramLogo size={48} weight="bold" />
